4b8e822ad7a63eca23939b9995759592f3ac1b04
dncortez14 <dncortez14@gmail.com>
2022-06-22 10:03:51 -0300

    JC-5 Connect to Room Chat

--- JobsityChat/Hubs/RoomChatHub.cs
+++ JobsityChat/Hubs/RoomChatHub.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace JobsityChat.Hubs
 {
-    public class RoomChatHub: Hub
+    public class RoomChatHub : Hub
     {
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", "pedro has join to the room");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", "pedro has left the room");
+        }
     }
 }
--- JobsityChat/wwwroot/js/roomchat.js
+++ JobsityChat/wwwroot/js/roomchat.js
@@ -2,8 +2,19 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/roomChat").build();
 
+connection.on("ReceiveMessage", function (message) {
+    alert(message);
+});
+
 connection.start().then(function () {
-    
+    AddToGroup();
 }).catch(function (err) {
     return console.error(err.toString());
 });
+
+function AddToGroup() {
+    var groupName = document.getElementById("Name").value;
+    connection.invoke("AddToGroup", groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
